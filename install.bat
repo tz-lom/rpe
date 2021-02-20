023f3b618da118e384e86eff7be9cbf05f4815b7
tz-lom <vzorvat@gmail.com>
2021-02-20 20:17:50 +0100

    Initial

---
+++ install.bat
@@ -0,0 +1,4 @@
+if not exist ./condaenv call conda create -q -y --prefix ./condaenv
+call conda activate ./condaenv
+call conda config --env --set subdir win-32
+call conda install -y -c bcilab resonance_mingw_32
