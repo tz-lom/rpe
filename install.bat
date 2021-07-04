7c17bd091e55d777b6ff0fddbb9222d0e44767fe
tz-lom <vzorvat@gmail.com>
2021-07-04 13:06:00 +0200

    Update version of resonance

--- install.bat
+++ install.bat
@@ -2,8 +2,8 @@ if not exist ./condaenv call conda create -q -y --prefix ./condaenv
 if not exist ./condaenv64 call conda create -q -y --prefix ./condaenv64 
 call conda activate ./condaenv
 call conda config --env --set subdir win-32
-call conda install -y -c bcilab resonance_mingw_32=3.4.1=20 resonance_pip=1.1.2=hfeaa757_10
+call conda install -y -c bcilab resonance_mingw_32=3.4.1=27 resonance_pip=1.1.2=hfeaa757_10
 
 call conda activate ./condaenv64
 call conda config --env --set subdir win-64
-call conda install -y -c bcilab resonance_mingw_64=3.4.1=20 resonance_pip=1.1.2=hfeaa757_10
+call conda install -y -c bcilab resonance_mingw_64=3.4.1=27 resonance_pip=1.1.2=hfeaa757_10
